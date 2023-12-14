cff6453242f4445f74d479eb064295cbb99e6041
Breanna Ranglall <114267416+ranglall@users.noreply.github.com>
2023-12-14 16:30:55 -0600

    qryMesostructureTypesSorted.py Prototype

---
+++ qryMesostructureTypesSorted.py
@@ -0,0 +1,44 @@
+import sqlite3
+import mysql.connector
+
+# JawsDB MySQL connection details
+db_config = {
+    'user': 'izgrmlgwk70csp9l',  # Your JawsDB username
+    'password': 'qpmikh9t3n3a2ekg',  # Your JawsDB password
+    'host': 'nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  # Your JawsDB host
+    'database': 'h762lahe056bge13',  # Your JawsDB database name
+    'port': 3306
+}
+
+# Open connection and create cursor
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+while True:
+    try:
+        MesoStructureTypeID = input("Enter What Mesostructure Type ID Info You Want:")
+        MesoStructureTypeID = int(MesoStructureTypeID)
+        break
+    except ValueError:
+        print("Entry Must Be An Integer!")
+
+MesoStructureTypeID = str(MesoStructureTypeID)
+print("You Entered: " + MesoStructureTypeID)
+
+qry = '''
+SELECT
+    MesoStructureTypeID,
+    MesoStructure
+FROM
+    MesoStructureType
+ORDER BY
+    MesoStructureTypeID;
+'''
+
+cursor.execute(qry)
+
+for row in cursor.fetchall():
+    print(row)
+    #print(", ".join(map(str, row)))
+
+cursor.close()
