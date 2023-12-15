68dfc2f9447557c70674d060c060c335fcd0222b
Jasper <138431971+Jespr0@users.noreply.github.com>
2023-12-14 20:13:36 -0600

    Update WaypointForm.py
    working insert statement

--- WaypointForm.py
+++ WaypointForm.py
@@ -1,225 +1,223 @@
-import sqlite3
+import mysql.connector
+from mysql.connector import Error
 
-#opens connection and creates cursor
-conn = sqlite3.connect('filler.db')
-cursor = conn.cursor()
+try:
+    # JawsDB MySQL connection details
+    db_config = {
+        'user': 'izgrmlgwk70csp9l',  # Your JawsDB username
+        'password': 'qpmikh9t3n3a2ekg',  # Your JawsDB password
+        'host': 'nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  # Your JawsDB host
+        'database': 'h762lahe056bge13',  # Your JawsDB database name
+        'port': 3306
+    }
 
-#wayptID Input
-while True:
-    try:
-        wayptID = input("Enter a Waypoint ID(Must Be An Integer): ")
-        wayptID = int(wayptID)
-        break
-    except ValueError:
-        print("Value Must Be An Integer!")
-wayptID = str(wayptID)
-print('Waypoint ID is: ' + wayptID)
-
-
-#wayptName Input
-while True:
-    try:
-        wayptName = input("Enter a Waypoint Name(Must Be A String): ")
-        wayptName = str(wayptName)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Waypoint Name is: ' + wayptName)
+    # Open connection
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
 
 
-#latitude Input
-while True:
-    try:
-        latitude = input("Enter A Latitude(Must Be A Float): ")
-        latitude = float(latitude)
-        break
-    except ValueError:
-        print("Value Must Be A Float!")
-latitude =str(latitude)
-print('Latitude is: ' + latitude)
+    #wayptName Input
+    while True:
+        try:
+            WayptName = input("Enter a Waypoint Name(Must Be A String): ")
+            WayptName = str(WayptName)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Waypoint Name is: ' + WayptName)
 
 
-#Longitude Input
-while True:
-    try:
-        longitude = input("Enter A Longitude(Must Be A Float): ")
-        longitude = float(longitude)
-        break
-    except ValueError:
-        print("Value Must Be A Float!")
-longitude =str(longitude)
-print('Latitude Is: ' + longitude)
+    #latitude Input
+    while True:
+        try:
+            Latitude = input("Enter A Latitude(Must Be A Float): ")
+            Latitude = float(Latitude)
+            break
+        except ValueError:
+            print("Value Must Be A Float!")
+    Latitude =str(Latitude)
+    print('Latitude is: ' + Latitude)
 
 
-#Northing Input
-while True:
-    try:
-        northing = input("Enter A Northing Value(Must Be A Float): ")
-        northing = float(northing)
-        break
-    except ValueError:
-        print("Value Must Be A Float!")
-northing =str(northing)
-print('Northing Is: ' + northing)
+    #Longitude Input
+    while True:
+        try:
+            Longitude = input("Enter A Longitude(Must Be A Float): ")
+            Longitude = float(Longitude)
+            break
+        except ValueError:
+            print("Value Must Be A Float!")
+    Longitude =str(Longitude)
+    print('Latitude Is: ' + Longitude)
+
+    #Zone1 Input
+    while True:
+        try:
+            UTMZone1 = input("Enter UMTZone1 (Must Be An Integer): ")
+            UTMZone1 = int(UTMZone1)
+            break
+        except ValueError:
+            print("Value Must Be An Integer!")
+    UTMZone1 = str(UTMZone1)
+    print('UMTZone1 Is: ' + UTMZone1)
 
 
-#Zone1 Input
-while True:
-    try:
-        zone1 = input("Enter UMTZone1 (Must Be An Integer): ")
-        zone1 = int(zone1)
-        break
-    except ValueError:
-        print("Value Must Be An Integer!")
-zone1 = str(zone1)
-print('UMTZone1 Is: ' + zone1)
+    #Zone2 Input
+    while True:
+        try:
+            UTMZone2 = input("Enter Zone 2 Value(Must Be A String): ")
+            UTMZone2 = str(UTMZone2)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('UMTZone2 Is: ' + UTMZone2)
 
 
-#Zone2 Input
-while True:
-    try:
-        zone2 = input("Enter Zone 2 Value(Must Be A String): ")
-        zone2 = str(zone2)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('UMTZone2 Is: ' + zone2)
+    #Datum Input
+    while True:
+        try:
+            Datum = input("Enter Datum (Must Be A String): ")
+            Datum = str(Datum)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Datum Is: ' + Datum)
 
 
-#Datum Input
-while True:
-    try:
-        datum = input("Enter Datum (Must Be A String): ")
-        datum = str(datum)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Datum Is: ' + datum)
+    #Projection Input
+    while True:
+        try:
+            Projection = input("Enter Projection (Must Be A String): ")
+            Projection = str(Projection)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Projection Is: ' + Projection)
 
 
-#Projection Input
-while True:
-    try:
-        projection = input("Enter Projection (Must Be A String): ")
-        projection = str(projection)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Projection Is: ' + projection)
+    #Fieldbook Input
+    while True:
+        try:
+            Fieldbook = input("Enter Fieldbook(Must Be A String): ")
+            Fieldbook = str(Fieldbook)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Fieldbook Is: ' + Fieldbook)
 
 
-#Fieldbook Input
-while True:
-    try:
-        fieldBook = input("Enter Fieldbook(Must Be A String): ")
-        fieldBook = str(fieldBook)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Fieldbook Is: ' + fieldBook)
+    #Page Input
+    while True:
+        try:
+            FieldbookPage = input("Enter Fieldbook Page(Must Be An Integer): ")
+            FieldbookPage = int(FieldbookPage)
+            break
+        except ValueError:
+            print("Value Must Be An Integer!")
+    FieldbookPage = str(FieldbookPage)
+    print('The Fieldbook Page Is: ' + FieldbookPage)
 
 
-#Page Input
-while True:
-    try:
-        page = input("Enter Fieldbook Page(Must Be An Integer): ")
-        page = int(page)
-        break
-    except ValueError:
-        print("Value Must Be An Integer!")
-page = str(page)
-print('The Fieldbook Page Is: ' + page)
+    #Formation Input
+    while True:
+        try:
+            Formation = input("Enter Formation(Must Be A String): ")
+            Formation = str(Formation)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Formation Is: ' + Formation)
 
 
-#Formation Input
-while True:
-    try:
-        formation = input("Enter Formation(Must Be A String): ")
-        formation = str(formation)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Formation Is: ' + formation)
+    #Site Name Input
+    while True:
+        try:
+            SiteOrLocationName = input("Enter Site Name(Must Be A String): ")
+            SiteOrLocationName = str(SiteOrLocationName)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Site Name Is: ' + SiteOrLocationName)
 
 
-#Site Name Input
-while True:
-    try:
-        siteName = input("Enter Site Name(Must Be A String): ")
-        siteName = str(siteName)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Site Name Is: ' + siteName)
+    #Date Collected Input
+    while True:
+        try:
+            DateCollected = input("Enter Date Collected(Format: Month-Day-Year/XX-XX-XXXX): ")
+            DateCollected = str(DateCollected)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Date Collected is: ' + DateCollected)
 
+    #Elevation Input
+    while True:
+        try:
+            Elevation = input("Enter the Elevation(Must Be A Float): ")
+            Elevation = float(Elevation)
+            break
+        except ValueError:
+            print("Value Must Be A Float!")
+    Elevation = str(Elevation)
+    print('Elevation is: ' + Elevation)
 
-#Date Collected Input
-while True:
-    try:
-        dateCollected = input("Enter Date Collected(Format: Month-Day-Year/XX-XX-XXXX): ")
-        dateCollected = str(dateCollected)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Date Collected is: ' + dateCollected)
+    #Project Input
+    while True:
+        try:
+            ProjectName = input("Enter Project(Must Be A String):")
+            ProjectName = str(ProjectName)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Project is: ' + ProjectName)
 
-#Elevation Input
-while True:
-    try:
-        elevation = input("Enter the Elevation(Must Be A Float): ")
-        elevation = float(elevation)
-        break
-    except ValueError:
-        print("Value Must Be A Float!")
-elevation = str(elevation)
-print('Elevation is: ' + elevation)
+    #Measured Section Input
+    MeasuredSection = ''
+    while MeasuredSection.upper() not in ['Y', 'N', 'y', 'n']:
+        MeasuredSection = input("Is The Section Measured? (Y/N)")
+    if MeasuredSection.upper() == 'Y': 
+        MeasuredSection = '1'
+    else:
+        MeasuredSection = '0'
+        
+    print("The Section Is Measured:", MeasuredSection)
 
-#Project Input
-while True:
-    try:
-        project = input("Enter Project(Must Be A String):")
-        project = str(project)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Project is: ' + project)
+    #Section Name Input
+    while True:
+        try:
+            SectionName = input("Enter Section Name(Must Be A String): ")
+            SectionName = str(SectionName)
+            break
+        except ValueError:
+            print("Value Must Be A String!")
+    print('Section Name Is: ' + SectionName)
 
-#Measured Section Input
-measuredSection = ''
-while measuredSection.upper() not in ['Y', 'N', 'y', 'n']:
-    measuredSection = input("Is The Section Measured? (Y/N)")
-measuredSection = measuredSection.upper() == 'Y'
-print("The Section Is Measured:", measuredSection)
+    #Section Comments
+    Comments = input("Add any comments if neccessary and press enter:")
+    Comments = str(Comments)
 
-#Section Name Input
-while True:
-    try:
-        sectionName = input("Enter Section Name(Must Be A String): ")
-        sectionName = str(sectionName)
-        break
-    except ValueError:
-        print("Value Must Be A String!")
-print('Section Name Is: ' + sectionName)
+    #Query String for execute
+    addWaypointQuery = '''
+    INSERT INTO Waypoint (
+         WayptName, Latitude, Longitude, UTMZone1, UTMZone2,
+        Datum, Projection, FieldBook, FieldbookPage, Formation, SiteOrLocationName, DateCollected,
+        Elevation, ProjectName, MeasuredSection, SectionName, Comments
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    '''
 
-#Section Comments
-comments = input("Add any comments if neccessary and press enter:")
+    #executes the cursor that is setup
+    cursor.execute(addWaypointQuery, (WayptName, Latitude, Longitude, UTMZone1, UTMZone2,
+                            Datum, Projection, Fieldbook, FieldbookPage, Formation, SiteOrLocationName, DateCollected,
+                            Elevation, ProjectName, MeasuredSection, SectionName, Comments))
 
-#Query String for execute
-addWaypointQuery = '''
-    INSERT INTO your_table_name (
-        wayptID, wayptName, latitude, longitude, northing, zone1, zone2,
-        datum, projection, fieldBook, page, formation, siteName, dateCollected,
-        elevation, project, measuredSection, sectionName, comments
-    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-'''
+    conn.commit()
+    print("Data inserted successfully.")
 
-#executes the cursor that is setup
-cursor.execute(addWaypointQuery, (wayptID, wayptName, latitude, longitude, northing, zone1, zone2,
-                           datum, projection, fieldBook, page, formation, siteName, dateCollected,
-                           elevation, project, measuredSection, sectionName, comments))
-
-#commits and closes connection
-conn.commit()
-conn.close()
-
-print(addWaypointQuery)
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if conn.is_connected():
+        cursor.close()
+        conn.close()
+        print("MySQL connection is closed")
+    
